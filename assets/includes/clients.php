cc6bb756c26a6a09d868c3d3dd59be014ff61c96
waleedamir571 <waleedamir063>
2024-09-13 20:05:53 +0500

    commitlast

---
+++ assets/includes/clients.php
@@ -0,0 +1,52 @@
+<section class="clients-section style-two" style="padding-top:35px;">
+    <div class="auto-container">
+        <div class="sec-title centered">
+            <h2>Our Accolades & Memberships</h2>
+        </div>
+        <div class="sponsors-outer">
+            <!--Sponsors Carousel-->
+            <ul class="sponsors-carousel owl-carousel owl-theme">
+                <li class="slide-item">
+                    <figure class="image-box"><img src="assets/images/clients/logo2.png" width="210" height="100" alt="" class="client-img img-responsive"></figure>
+                </li>
+                 <li class="slide-item">
+                    <figure class="image-box"><img src="assets/images/clients/01.webp" width="210" height="100" alt="" class="client-img img-responsive"></figure>
+                </li>
+                <li class="slide-item">
+                    <figure class="image-box"><img src="assets/images/clients/02.webp" alt="" width="210" height="100"  class="client-img img-responsive"></figure>
+                </li>
+                <li class="slide-item">
+                    <figure class="image-box"><img src="assets/images/clients/03.webp" alt="" width="210" height="100" class="client-img img-responsive"></figure>
+                </li>
+                <li class="slide-item">
+                    <figure class="image-box"><img src="assets/images/clients/011.jpg" alt="" width="210" height="100" class="client-img img-responsive"></figure>
+                </li>
+                <li class="slide-item">
+                    <figure class="image-box"><img src="assets/images/clients/012.jpg" alt="" width="210" height="100" class="client-img img-responsive"></figure>
+                </li>
+                <li class="slide-item">
+                    <figure class="image-box"><img src="assets/images/clients/013.jpg" alt="" width="210" height="100" class="client-img img-responsive"></figure>
+                </li>
+                <li class="slide-item">
+                    <figure class="image-box"><img src="assets/images/clients/014.jpg" alt="" width="210" height="100" class="client-img img-responsive"></figure>
+                </li>
+                <li class="slide-item">
+                    <figure class="image-box"><img src="assets/images/clients/05.webp" alt="" width="100" height="100" class="client-img img-responsive"></figure>
+                </li>
+        
+                <li class="slide-item">
+                    <figure class="image-box"><img src="assets/images/clients/07.webp" alt="" width="114" height="100" class="client-img img-responsive"></figure>
+                </li>
+                <li class="slide-item">
+                    <figure class="image-box"><img src="assets/images/clients/08.jpg" alt="" width="210" height="100" class="client-img img-responsive"></figure>
+                </li>
+                <li class="slide-item">
+                    <figure class="image-box"><img src="assets/images/clients/09.png" alt="" width="210" height="100" class="client-img img-responsive"></figure>
+                </li>
+                 <li class="slide-item">
+                    <figure class="image-box"><img src="assets/images/clients/image001.jpg" alt="" width="210" height="90" class="client-img img-responsive"></figure>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
